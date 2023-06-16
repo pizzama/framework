0c14773fe78bc9fb9d07bafb54c91428b53d8960
bin.sun <osunkingo@sina.com>
2023-06-17 00:34:03 +0800

    add safe area

---
+++ Assets/SFramework/Script/Utils/UI/SafeArea.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeArea : MonoBehaviour
+{
+    [SerializeField] private bool _left;
+    [SerializeField] private bool _right;
+    [SerializeField] private bool _top;
+    [SerializeField] private bool _bottom;
+
+    private void Start()
+    {
+        var panel = GetComponent<RectTransform>();
+        var area = Screen.safeArea;
+
+        var anchorMin = area.position;
+        var anchorMax = area.position + area.size;
+
+        if (_left) anchorMin.x /= Screen.width;
+        else anchorMin.x = 0;
+
+        if (_right) anchorMax.x /= Screen.width;
+        else anchorMax.x = 1;
+
+        if (_bottom) anchorMin.y /= Screen.height;
+        else anchorMin.y = 0;
+
+        if (_top) anchorMax.y /= Screen.height;
+        else anchorMax.y = 1;
+
+        panel.anchorMin = anchorMin;
+        panel.anchorMax = anchorMax;
+    }
+}
