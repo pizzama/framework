77d9c0713d418d58dc064cd4fe929970e143f90c
bin.sun <osunkingo@sina.com>
2023-08-11 12:24:49 +0800

    update controller

--- Assets/SFramework/Script/Game/Actor/SFActorController.cs
+++ Assets/SFramework/Script/Game/Actor/SFActorController.cs
@@ -5,17 +5,29 @@ using UnityEngine.UIElements;
 
 namespace SFramework.Actor
 {
+    public enum SFActorUpdateModes { Update, FixedUpdate }
     public class SFActorController : MonoBehaviour
     {
-        [SerializeField] private Vector3 _currentDirection;
-        [SerializeField] private bool _isGrounded;
+        [SerializeField] protected Vector3 currentDirection;
+        [SerializeField] protected bool isGrounded;
+        [SerializeField] protected Vector3 speed;
+        [Header("Gravity")]
+        [SerializeField] protected float gravity = -30f;
+        [SerializeField] protected bool isActiveGravity = true;
+        [SerializeField] protected Vector3 currentMovement;
+        /// whether or not the character is in free movement mode or not
+        [SerializeField] protected bool freeMovement = true;
+
+        protected Vector3 _lastUpdatePosition;
         protected virtual void Awake()
         {
-            _currentDirection = transform.forward;
+            currentDirection = transform.forward;
         }
         protected virtual void Update()
         {
-
+            checkIsGrounded();
+            handleFriction();
+            determineDirection();
         }
 
         protected virtual void LateUpdate()
@@ -24,17 +36,41 @@ namespace SFramework.Actor
         }
         protected virtual void FixedUpdate()
         {
-
+            computeSpeed();
         }
         protected void checkIsGrounded()
         {
 
         }
+        protected virtual void handleFriction()
+        {
+
+        }
+        protected virtual void determineDirection()
+        {
+
+        }
+        public virtual void SetMovement(Vector3 movement)
+        {
+
+        }
+        protected virtual void computeSpeed()
+        {
+            speed = (this.transform.position - _lastUpdatePosition) / Time.deltaTime;
+            // we round the speed to 2 decimals
+            speed.x = Mathf.Round(speed.x * 100f) / 100f;
+            speed.y = Mathf.Round(speed.y * 100f) / 100f;
+            speed.z = Mathf.Round(speed.z * 100f) / 100f;
+            _lastUpdatePosition = this.transform.position;
+        }
 
         public virtual void Reset()
         {
-            _currentDirection = Vector3.zero;
-            _isGrounded = true;
+            currentDirection = Vector3.zero;
+            isGrounded = true;
+            speed = Vector3.zero;
+            isActiveGravity = true;
+            currentMovement = Vector3.zero;
         }
     }
 }
--- Assets/SFramework/Script/Game/Actor/SFActorController3D.cs
+++ Assets/SFramework/Script/Game/Actor/SFActorController3D.cs
@@ -11,12 +11,68 @@ namespace SFramework.Actor
         [SerializeField] private CharacterController _characterController;
         [SerializeField] protected Rigidbody _rigidBody;
         [SerializeField] protected Collider _collider;
+        [SerializeField] private SFActorUpdateModes _updateMode;
+
+        private Vector3 _inputMoveDirection;
+        private Vector3 _newVelocity;
         protected override void Awake()
         {
             base.Awake();
             _characterController = GetComponent<CharacterController>();
             _rigidBody = GetComponent<Rigidbody>();
             _collider = GetComponent<Collider>();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            if (_updateMode == SFActorUpdateModes.Update)
+            {
+                processUpdate();
+            }
+        }
+
+        //计算新的位移，并移动角色
+        protected virtual void processUpdate()
+        {
+            if (transform == null)
+            {
+                return;
+            }
+
+            if (!freeMovement)
+            {
+                return;
+            }
+
+            addInput();
+        }
+
+        public override void SetMovement(Vector3 movement)
+        {
+            currentMovement = movement;
+
+            Vector3 directionVector;
+            directionVector = movement;
+            if (directionVector != Vector3.zero)
+            {
+                float directionLength = directionVector.magnitude;
+                directionVector = directionVector / directionLength;
+                directionLength = Mathf.Min(1, directionLength);
+                directionLength = directionLength * directionLength;
+                directionVector = directionVector * directionLength;
+            }
+            _inputMoveDirection = transform.rotation * directionVector;
+        }
+
+        protected virtual void addInput()
+        {
+
+        }
+
+        protected virtual void addGravity()
+        {
+
         }
     }
 }
