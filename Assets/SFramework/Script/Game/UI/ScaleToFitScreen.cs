c401cf72c1d2d4d852cb6756ef3c523927ee33e4
bin.sun <pizzaman@126.com>
2023-08-26 18:04:16 +0800

    update screen fix

--- Assets/SFramework/Script/Game/UI/ScaleToFitScreen.cs
+++ Assets/SFramework/Script/Game/UI/ScaleToFitScreen.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
 namespace SFramework.UI
 {
-    [ExecuteInEditMode]
+    [ExecuteAlways]
     public class ScaleToFitScreen : MonoBehaviour
     {
         private SpriteRenderer sr;
 
         private void Start()
         {
+            // world height is always camera's orthographicSize * 2
             sr = GetComponent<SpriteRenderer>();
 
-            // 世界的高永远是相机正交大小的2倍
+            // world width is calculated by diving world height with screen heigh
+            // then multiplying it with screen width
             float worldScreenHeight = Camera.main.orthographicSize * 2;
 
-            // 世界的宽=世界的高/屏幕的高*屏幕的宽
+            // world width is calculated by diving world height with screen heigh
+            // then multiplying it with screen width
             float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
 
+            // to scale the game object we divide the world screen width with the
+            // size x of the sprite, and we divide the world screen height with the
+            // size y of the sprite
             transform.localScale = new Vector3(
                 worldScreenWidth / sr.sprite.bounds.size.x,
                 worldScreenHeight / sr.sprite.bounds.size.y, 1);
