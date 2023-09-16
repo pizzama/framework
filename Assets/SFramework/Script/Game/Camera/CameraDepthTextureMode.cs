019d79d25ce50ed2ed9ccd6efe0f61786c6930cc
bin.sun <pizzaman@126.com>
2023-09-17 00:03:03 +0800

    移动camera

---
+++ Assets/SFramework/Script/Game/Camera/CameraDepthTextureMode.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace SFramework.CameraUtils
+{
+
+    //设置摄像机的纹理深度
+    [RequireComponent(typeof(Camera))]
+    public class CameraDepthTextureMode : MonoBehaviour
+    {
+        [SerializeField]
+        DepthTextureMode depthTextureMode;
+
+        private void OnValidate()
+        {
+            SetCameraDepthTextureMode();
+        }
+
+        private void Awake()
+        {
+            SetCameraDepthTextureMode();
+        }
+
+        private void SetCameraDepthTextureMode()
+        {
+            GetComponent<Camera>().depthTextureMode = depthTextureMode;
+        }
+    }
+}
