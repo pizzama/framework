19e8d391fc71dece163b80f8d3f45d1f411ded98
bin.sun <pizzaman@126.com>
2023-09-26 12:40:13 +0800

    增加清理bundle名字的工具

---
+++ Assets/SFramework/Editor/ResCleanAssetsMenu.cs
@@ -0,0 +1,94 @@
+using System;
+using UnityEditor;
+using System.IO;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace SFramework
+{
+	[InitializeOnLoad]
+	public class ResCleanAssetsMenu
+	{
+		private const string StaticlNameSpace = "SFramework.Statics";
+		private const string Mark_AssetBundle = "Assets/SFramework/AssetBundle Clean Folder";
+		private const string StaticClassName = "SFResAssets";
+		private const string StaticPath = "SFStaticAsset";
+
+		[MenuItem(Mark_AssetBundle)]
+		public static void MarkPTABDir()
+		{
+			string assetDirectory = "Assets/Arts";
+			DirectoryInfo directoryInfo = new DirectoryInfo(assetDirectory);
+			DirectoryInfo[] scenesDirectories = directoryInfo.GetDirectories();
+			foreach (var tempDir in scenesDirectories)
+			{
+				string sceneDirectory = assetDirectory + "/" + tempDir.Name;
+				DirectoryInfo sceneDirectoryInfo = new DirectoryInfo(sceneDirectory);
+				if (sceneDirectoryInfo == null)
+				{
+					Debug.Log(sceneDirectoryInfo + "not exists");
+					return;
+				}
+				else
+				{
+					Dictionary<string, string> namePathDictionary = new Dictionary<string, string>();
+					int index = sceneDirectory.LastIndexOf("/");
+					string sceneName = sceneDirectory.Substring(index + 1);
+					OnSceneFileSystemInfo(sceneDirectoryInfo, sceneName, namePathDictionary);
+					//OnWriteConfig(sceneName, namePathDictionary);// write template file
+				}
+			}
+			AssetDatabase.Refresh();
+			AssetDatabase.RemoveUnusedAssetBundleNames();
+			Debug.Log("bundle setting over...");
+		}
+
+		private static void WriteDataConfig()
+		{
+			var path = Path.GetFullPath(Application.dataPath + Path.DirectorySeparatorChar + StaticPath);
+			if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+			path = path + "/" + StaticClassName + ".cs";
+			var writer = new StreamWriter(File.Open(path, FileMode.Create));
+			ResDataCodeGenerator.WriteClass(writer, StaticlNameSpace, StaticClassName);
+			writer.Close();
+			AssetDatabase.Refresh();
+		}
+
+
+		private static void OnSceneFileSystemInfo(FileSystemInfo fileSystemInfo, string sceneNama, Dictionary<string, string> namePathDictionary)
+		{
+			if (!fileSystemInfo.Exists)
+			{
+				Debug.Log(fileSystemInfo + "not exists");
+				return;
+			}
+			DirectoryInfo directoryInfo = fileSystemInfo as DirectoryInfo;
+
+			FileSystemInfo[] fileSystemInfos = directoryInfo.GetFileSystemInfos();
+			foreach (var systemInfo in fileSystemInfos)
+			{
+				FileInfo fileInfo = systemInfo as FileInfo;
+				if (fileInfo == null)
+				{
+					OnSceneFileSystemInfo(systemInfo, sceneNama, namePathDictionary);
+				}
+				else
+				{
+					SetLables(fileInfo, sceneNama, namePathDictionary);
+				}
+			}
+		}
+
+		/// <summary>
+		/// ÐÞ¸Ä×ÊÔ´ assetbundle lables
+		/// </summary>
+		private static void SetLables(FileInfo fileInfo, string sceneName, Dictionary<string, string> namePathDictionary)
+		{
+			int index = fileInfo.FullName.IndexOf("Assets");
+			string assetPath = fileInfo.FullName.Substring(index);
+			AssetImporter assetImporter = AssetImporter.GetAtPath(assetPath);
+			if(assetImporter != null)
+				assetImporter.assetBundleName = null;
+		}
+	}
+}
