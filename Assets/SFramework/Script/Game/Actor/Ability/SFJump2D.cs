62fb047e33a6e56d47d83ba36369f1b22598d52f
bin.sun <pizzaman@126.com>
2023-10-16 21:00:14 +0800

    增加弹跳能力

--- Assets/SFramework/Script/Game/Actor/Ability/SFJump2D.cs
+++ Assets/SFramework/Script/Game/Actor/Ability/SFJump2D.cs
@@ -9,6 +9,12 @@ namespace SFramework.Actor.Ability
 	/// </summary>
 	public class SFJump2D : SFAbility
 	{
+        public bool JumpProportionalToPress = true; // whether or not the jump should be proportional to press (if yes, releasing the button will stop the jump)
+        public float MinimumPressTime = 0.4f;// the minimum amount of time after the jump's start before releasing the jump button has any effect
+        public float JumpForce = 800f; // the force to apply to the jump, the higher the jump, the faster the jump
+        public float JumpHeight = 4f; // the height the jump should have
+        public bool CanJumpOnTooSteepSlopes = true;
+        public bool ResetJumpsOnTooSteepSlopes = false;
 		protected override void init()
 		{
 		}
