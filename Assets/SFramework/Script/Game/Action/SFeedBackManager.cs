2c50c20fbd892cc5f3215c97addec012c7e8543f
bin.sun <pizzaman@126.com>
2023-11-03 20:36:42 +0800

    增加feedback管理器

--- Assets/SFramework/Script/Game/Action/SFeedBackManager.cs
+++ Assets/SFramework/Script/Game/Action/SFeedBackManager.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SFeedBackManager : MonoBehaviour
+namespace SFramework.Action
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SFeedBackManager : MonoBehaviour
     {
-        
-    }
+        public List<SFeedBack> Feedbacks = new List<SFeedBack>();
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
