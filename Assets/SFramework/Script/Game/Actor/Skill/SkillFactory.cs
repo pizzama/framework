42d478539a5ed469fc47adb15184afe8f375454c
bin.sun <pizzaman@126.com>
2023-11-10 21:26:57 +0800

    增加技能工厂

--- Assets/SFramework/Script/Game/Actor/Skill/SkillFactory.cs
+++ Assets/SFramework/Script/Game/Actor/Skill/SkillFactory.cs
@@ -1,9 +1,17 @@
 using System;
-using UnityEngine;
 
 public sealed class SkillFactory
 {
     private static readonly Lazy<SkillFactory> lazy =
         new Lazy<SkillFactory>(() => new SkillFactory());
     public static SkillFactory Instance { get { return lazy.Value; } }
+    private SkillFactory()
+    {
+        
+    }
+
+    private void initAllScript()
+    {
+
+    }
 }
