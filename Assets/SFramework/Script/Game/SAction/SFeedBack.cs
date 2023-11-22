4f0cd6822df3965253392df163459d33c7230ea8
bin.sun <pizzaman@126.com>
2023-11-22 23:05:33 +0800

    增加fbtiming

--- Assets/SFramework/Script/Game/SAction/SFeedBack.cs
+++ Assets/SFramework/Script/Game/SAction/SFeedBack.cs
@@ -7,6 +7,16 @@ namespace SFramework.SAction
     public class SFeedBack : MonoBehaviour
     {
         public enum SafeModes { Nope, EditorOnly, RuntimeOnly, Full }
+
+        [Tooltip("在inspector里显示这个组件的名字")]
+        public string Label = "SFeedback";
+        [Tooltip("FeedBack发生的概率 100完全发生, 0完全不发生依次类推")]
+        public float Probability = 100f;
+        public SFeedBackTiming Timing;
+
+#if UNITY_EDITOR
+        public virtual Color FeedbackColor { get { return Color.white; } }
+#endif
         void Start()
         {
 
