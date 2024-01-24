e6d5f463fe9b23720bd4d0b410f64bb603d84bc2
bin.sun <pizzaman@126.com>
2024-01-24 18:51:53 +0800

    修改prefab

---
+++ Assets/SFramework/Editor/SControlCreator.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class SControlCreator : EditorWindow
+{
+    private string pathText = "";
+    [MenuItem("SFrameWork/Editor/SFramework Control Creator", false, 100)]
+    public static void Open()
+    {
+        var window = GetWindow<SControlCreator>($"Control Creator");
+        window.Show();
+    }
+
+
+    private void OnGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.Space();
+        bool pathFold = true;
+        pathText = ""; //output parent path
+        pathFold = EditorGUILayout.BeginFoldoutHeaderGroup(pathFold, "Set Out Path");
+        if (pathFold)
+        {
+            EditorGUILayout.LabelField("Input Path");
+            pathText = EditorGUILayout.TextArea(pathText);
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Create"))
+        {
+            createFolder();
+        }
+
+        bool outFolder = false;
+        outFolder = EditorGUILayout.BeginFoldoutHeaderGroup(outFolder, "Result Info");
+        if (outFolder)
+        {
+            Vector2 scrollPosition = Vector2.zero;
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, true, true);
+
+            EditorGUILayout.EndScrollView();
+        }
+        EditorGUILayout.EndFoldoutHeaderGroup();
+        EditorGUILayout.EndVertical();
+    }
+
+    private void createFolder()
+    {
+        string parentPath = Application.dataPath + "/Script/" + pathText;
+        if (!Directory.Exists(parentPath))
+        {
+            Directory.CreateDirectory(parentPath);
+        }
+    }
+}
