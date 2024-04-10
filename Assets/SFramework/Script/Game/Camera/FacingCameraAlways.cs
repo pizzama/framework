5dd5e45b469162fff63bcc6ebb842f17595f6ef2
bin.sun <pizzaman@126.com>
2024-04-10 14:00:53 +0800

    增加摄像机镜头朝向的设置

--- Assets/SFramework/Script/Game/Camera/FacingCameraAlways.cs
+++ Assets/SFramework/Script/Game/Camera/FacingCameraAlways.cs
@@ -6,8 +6,18 @@ namespace SFramework.CameraUtils
     public class FacingCameraAlways : MonoBehaviour
     {
         private Transform[] _childs;
-        [SFReadOnly][SerializeField] private int _totalChildren;
-        [SFReadOnly][SerializeField] private Quaternion _rotation;
+
+        [SFReadOnly]
+        [SerializeField]
+        private int _totalChildren;
+
+        [SFReadOnly]
+        [SerializeField]
+        private Quaternion _rotation;
+
+        [SerializeField]
+        private Camera _camera;
+
         private void Start()
         {
             Collect();
@@ -15,11 +25,14 @@ namespace SFramework.CameraUtils
 
         private void Update()
         {
-            for (int i = 0; i < _childs.Length; i++)
+            if (_camera != null)
             {
-                _rotation = Camera.main.transform.rotation;
-                _childs[i].rotation = _rotation;
-                _totalChildren = _childs.Length;
+                for (int i = 0; i < _childs.Length; i++)
+                {
+                    _rotation = _camera.transform.rotation;
+                    _childs[i].rotation = _rotation;
+                    _totalChildren = _childs.Length;
+                }
             }
         }
 
@@ -32,6 +45,8 @@ namespace SFramework.CameraUtils
             }
 
             _totalChildren = _childs.Length;
+            if (_camera == null)
+                _camera = Camera.main;
         }
     }
 }
