c4651589e4ba0543266cda5868ab60a9435d3ab0
bin.sun <pizzaman@126.com>
2024-04-12 22:59:52 +0800

    迁移资源

---
+++ Assets/Script/App/Farm/ShadowCtr.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowCtr : MonoBehaviour {
+
+    private LineRenderer line;
+	void Start () {
+        line = gameObject.GetComponent<LineRenderer>();
+
+        line.transform.rotation = Quaternion.LookRotation(new Vector3(0, 0.5f, 0));
+	}
+	
+
+	void Update () {
+		
+	}
+}
