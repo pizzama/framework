b07973c117680850dd0dc49fd530d0dcdef9ee51
bin.sun <pizzaman@126.com>
2024-04-23 12:19:48 +0800

    增加fps显示

---
+++ Assets/SFramework/Script/Game/UI/SFPSText.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SFramework.UI
+{
+    public class SFPSText : MonoBehaviour
+    {
+        [SerializeField] Text _framePerSecondText;
+        
+        private float _deltaTime;
+
+        void Update()
+        {
+            _deltaTime += (Time.deltaTime - _deltaTime) * 0.1f;
+            _framePerSecondText.text = "FPS " + Mathf.Ceil(1f / m_DeltaTime).ToString();
+        }
+    }
+}
