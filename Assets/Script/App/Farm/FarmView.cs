633db8292c4bea06c0996e67519e50705b846a66
bin.sun <pizzaman@126.com>
2024-04-24 23:05:30 +0800

    增加grid

--- Assets/Script/App/Farm/FarmView.cs
+++ Assets/Script/App/Farm/FarmView.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using SFramework;
 using SFramework.Game;
 using UnityEngine.UI;
+using SFramework.Game.Map;
 
 namespace App.Farm
 {
@@ -14,6 +15,7 @@ namespace App.Farm
 		protected override void opening()
 		{
 			// Code Here
+			SMGrid grid = new SMGrid(10, 10, 3, new Vector3(0,0,0));
 		}
 		protected override void closing()
 		{
