288333b7f4bb369cf104bdc46fc60959ef7520e1
bin.sun <pizzaman@126.com>
2024-04-26 20:26:28 +0800

    增加纹理处理

--- Assets/Script/App/Farm/FarmView.cs
+++ Assets/Script/App/Farm/FarmView.cs
@@ -3,11 +3,13 @@ using SFramework;
 using SFramework.Game;
 using UnityEngine.UI;
 using SFramework.Game.Map;
+using SFramework.Tools;
 
 namespace App.Farm
 {
 	public class FarmView : SSCENEView
 	{
+		private SMGrid _grid;
 		protected override ViewOpenType GetViewOpenType()
 		{
 			return ViewOpenType.Single;
@@ -15,11 +17,22 @@ namespace App.Farm
 		protected override void opening()
 		{
 			// Code Here
-			SMGrid grid = new SMGrid(10, 10, 3, new Vector3(0,0,0));
+			_grid = new SMGrid(10, 10, 3, new Vector3(0,0,0));
 		}
+
 		protected override void closing()
 		{
 			// Code Here
+		}
+
+		protected override void viewUpdate()
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				Vector3 vec = MapTools.GetMouseWorldPosition();
+				Debug.Log(vec);
+				_grid.SetValue(vec, 100);
+			}
 		}
 	}
 }
