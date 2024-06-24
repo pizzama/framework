ef2292b41f554087a53cb7bd2d495ae0a709d132
bin.sun <pizzaman@126.com>
2024-06-24 09:40:10 +0800

    增加python

--- mod_team/battle/buffs/buf_control.py
+++
@@ -1,6 +0,0 @@
-from game.core.mods.mod_team.battle.buffs.buff import Buff
-
-
-class buf_control(Buff):  # 免控
-    def care_group(self):
-        return [20]
--- mod_team/battle/buffs/buf_silent.py
+++
@@ -1,6 +0,0 @@
-from game.core.mods.mod_team.battle.buffs.buff import Buff
-
-
-class buf_silent(Buff):  # 沉默
-    def pre_execute(self):
-        return {"silent": 1}
