75dcad6d2fdf32d04c0c9782f45699c1d4567099
bin.sun <pizzaman@126.com>
2024-07-02 02:24:45 +0800

    增加异步读取文件

--- Assets/SFramework/Script/Game/App/Root/RootModel.cs
+++ Assets/SFramework/Script/Game/App/Root/RootModel.cs
@@ -1,8 +1,10 @@
 using System;
-using Cysharp.Threading.Tasks;
-using UnityEngine;
-using Google.Protobuf;
 using System.IO;
+using Cysharp.Threading.Tasks;
+using Google.Protobuf;
+using UnityEngine;
+using UnityEngine.Networking;
+
 namespace SFramework.Game
 {
     public class RootModel : SModel
@@ -15,7 +17,8 @@ namespace SFramework.Game
             base.Install();
         }
 
-        protected void SaveData<T>(T data, string fileName) where T : Google.Protobuf.IMessage
+        protected void SaveData<T>(T data, string fileName)
+            where T : Google.Protobuf.IMessage
         {
             try
             {
@@ -40,7 +43,8 @@ namespace SFramework.Game
             }
         }
 
-        public T ReadData<T>(string fileName) where T : Google.Protobuf.IMessage, new()
+        public T ReadData<T>(string fileName)
+            where T : Google.Protobuf.IMessage, new()
         {
             try
             {
@@ -61,33 +65,66 @@ namespace SFramework.Game
             }
 
             return new T();
-
         }
 
-        public void SaveData<T>(T data) where T : Google.Protobuf.IMessage, new()
+        public T ReadData<T>()
+            where T : Google.Protobuf.IMessage, new()
+        {
+            Type tp = typeof(T);
+            return ReadData<T>(tp.FullName + ".bytes");
+        }
+
+        public void SaveData<T>(T data)
+            where T : Google.Protobuf.IMessage, new()
         {
             Type tp = typeof(T);
             SaveData(data, tp.FullName + ".bytes");
         }
 
-        public T ReadData<T>() where T :Google.Protobuf.IMessage, new()
+        public async UniTask<T> ReadDataAsync<T>()
+            where T : Google.Protobuf.IMessage, new()
         {
             Type tp = typeof(T);
-            return ReadData<T>(tp.FullName + ".bytes");
+            return await ReadDataAsync<T>(tp.FullName + ".bytes");
+        }
+
+        public async UniTask<T> ReadDataAsync<T>(string fileName)
+            where T : Google.Protobuf.IMessage, new()
+        {
+            try
+            {
+                string fileFullPath = Application.persistentDataPath + "/" + fileName;
+                UnityWebRequest request = UnityWebRequest.Get(fileFullPath);
+                await request.SendWebRequest();
+                if (request.isDone)
+                {
+                    T dt = GetProtobufObjectFromBytes<T>(request.downloadHandler.data);
+                    return dt;
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError(e);
+            }
+
+            return new T();
         }
 
         private byte[] GetBytesFromProtoObject(Google.Protobuf.IMessage msg)
         {
             using (MemoryStream sndms = new MemoryStream())
             {
-                Google.Protobuf.CodedOutputStream cos = new Google.Protobuf.CodedOutputStream(sndms);
+                Google.Protobuf.CodedOutputStream cos = new Google.Protobuf.CodedOutputStream(
+                    sndms
+                );
                 cos.WriteMessage(msg);
                 cos.Flush();
                 return sndms.ToArray();
             }
         }
 
-        private T GetProtobufObjectFromBytes<T>(byte[] bytes) where T : Google.Protobuf.IMessage, new()
+        private T GetProtobufObjectFromBytes<T>(byte[] bytes)
+            where T : Google.Protobuf.IMessage, new()
         {
             Google.Protobuf.CodedInputStream cis = new Google.Protobuf.CodedInputStream(bytes);
             T msg = new T();
@@ -95,6 +132,4 @@ namespace SFramework.Game
             return msg;
         }
     }
-
-
 }
