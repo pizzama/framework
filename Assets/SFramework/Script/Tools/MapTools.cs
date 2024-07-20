c8f134b680cfe9efa9ce49a2ac3d7fbd16f08606
bin.sun <pizzaman@126.com>
2024-07-21 07:29:47 +0800

    增加坐标系转换

--- Assets/SFramework/Script/Tools/MapTools.cs
+++ Assets/SFramework/Script/Tools/MapTools.cs
@@ -5,28 +5,41 @@ namespace SFramework.Tools
     public class MapTools
     {
         // Get Mouse Position in World with Z = 0f
-        public static Vector3 GetMouseWorldPosition() {
+        public static Vector3 GetMouseWorldPosition()
+        {
             Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
             vec.z = 0f;
             return vec;
         }
 
-        public static Vector3 GetMouseWorldPositionWithZ() {
+        public static Vector3 GetMouseWorldPositionWithZ()
+        {
             return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
         }
 
-        public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera) {
+        public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+        {
             return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
         }
 
-        public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera) {
+        public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+        {
             Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
             return worldPosition;
         }
 
-        public static Vector3 GetDirToMouse(Vector3 fromPosition) {
+        public static Vector3 GetDirToMouse(Vector3 fromPosition)
+        {
             Vector3 mouseWorldPosition = GetMouseWorldPosition();
             return (mouseWorldPosition - fromPosition).normalized;
+        }
+
+        public static Vector3 Screen2WorldPos(Vector3 pos, Camera worldCamera)
+        {
+            pos.z = 0;
+            var viewRay = worldCamera.ScreenPointToRay(pos);
+            var worldPos = viewRay.GetPoint(-viewRay.origin.z / viewRay.direction.z);
+            return worldPos;
         }
     }
 }
