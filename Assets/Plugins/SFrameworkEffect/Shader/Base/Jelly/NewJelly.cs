91656182ac0e1d00ed449a6b87b7ddbb8d76d904
bin.sun <pizzaman@126.com>
2024-08-23 22:17:43 +0800

    【框架】迁移资源

---
+++ Assets/Plugins/SFrameworkEffect/Shader/Base/Jelly/NewJelly.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NewJelly : MonoBehaviour
+{
+    private Material newJellyMat;
+    public Vector3 lastPostion;
+    public float stiffness = 1;
+    public float damping = 0.75f;
+    public float mass = 1;
+    public Vector3 velocity = new Vector3(0, 0, 0);
+    public Vector3 Force;
+    public Vector3 abstractPos;
+
+    private void Awake()
+    {
+        newJellyMat = GetComponent<Image>().material;
+        // newJellyMat = GetComponent<MeshRenderer>().material;
+        lastPostion = transform.position;
+        abstractPos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        newJellyMat.SetFloat("_SizeY", GetComponent<RectTransform>().rect.max.y);
+        // newJellyMat.SetFloat("_SizeY", GetComponent<MeshRenderer>().bounds.max.y);
+        Force = (lastPostion - abstractPos) * stiffness;
+        velocity = (velocity + Force / mass) * damping;
+        abstractPos += velocity;
+        newJellyMat.SetVector("_AbstracPos", transform.InverseTransformPoint(abstractPos));
+        lastPostion = transform.position;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(abstractPos, 0.01f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(abstractPos, velocity + abstractPos);
+    }
+}
