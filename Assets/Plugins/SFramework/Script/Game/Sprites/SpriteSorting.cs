b72fc44318e0a66dcf1419740cbe177b36488f4d
bin.sun <pizzaman@126.com>
2024-09-05 17:14:08 +0800

    [通用] 处理框架 修改有sortinggroup和spriterender和particlesystemrender的排序

--- Assets/Plugins/SFramework/Script/Game/Sprites/SpriteSorting.cs
+++ Assets/Plugins/SFramework/Script/Game/Sprites/SpriteSorting.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using SFramework.Extension;
 using UnityEngine;
+using UnityEngine.Rendering;
 using static SEnum;
 
 namespace SFramework.Sprites
@@ -16,34 +17,52 @@ namespace SFramework.Sprites
         [SerializeField] private SAxis _sortAxis = SAxis.Y;
 
         private List<SpriteRenderer> _sprites;
+        private List<ParticleSystemRenderer> _particleSystem;
+        private List<SortingGroup> _sortingGroups;
 
         private void OnEnable()
         {
             if (_cameraTransform == null)
                 _cameraTransform = Camera.main.transform;
-
-            _sprites = this.transform.CollectAllComponent<SpriteRenderer>();
+            _sortingGroups = this.transform.CollectAllComponent<SortingGroup>();
+            if (_sortingGroups.Count > 0)
+            {
+                _sprites = this.transform.CollectAllComponent<SpriteRenderer>();
+                _particleSystem = this.transform.CollectAllComponent<ParticleSystemRenderer>();
+            }
         }
         private void LateUpdate()
         {
             Vector3 pos = transform.position;
             if (pos != _worldPosition)
             {
-                if (_sprites.Count > 0)
+                if (_sprites != null && _sprites.Count > 0)
                 {
                     for (int i = 0; i < _sprites.Count; i++)
                     {
-                        SpriteRenderer spriteRenderer = _sprites[i];
-                        float sort;
-                        if (_sortAxis == SAxis.X)
-                            sort = Mathf.Abs(_cameraTransform.position.x - transform.position.x) * 16;
-                        else if (_sortAxis == SAxis.Y)
-                            sort = Mathf.Abs(_cameraTransform.position.y - transform.position.y) * 16;
-                        else
-                            sort = Mathf.Abs(_cameraTransform.position.z - transform.position.z) * 16;
+                        SpriteRenderer render = _sprites[i];
+                        float sort = caculateSort(_cameraTransform);
+                        render.sortingOrder = Mathf.CeilToInt(-sort + _offset);
+                    }
+                }
 
-                        spriteRenderer.sortingOrder = Mathf.CeilToInt(-sort + _offset);
-                        //Debug.Log(spriteRenderer.name + ";" + spriteRenderer.sortingOrder + ";" + spriteRenderer.transform.position);
+                if (_particleSystem != null && _particleSystem.Count > 0)
+                {
+                    for (int i = 0; i < _particleSystem.Count; i++)
+                    {
+                        ParticleSystemRenderer render = _particleSystem[i];
+                        float sort = caculateSort(_cameraTransform);
+                        render.sortingOrder = Mathf.CeilToInt(-sort + _offset);
+                    }
+                }
+                
+                if (_sortingGroups != null && _sortingGroups.Count > 0)
+                {
+                    for (int i = 0; i < _sortingGroups.Count; i++)
+                    {
+                        SortingGroup render = _sortingGroups[i];
+                        float sort = caculateSort(_cameraTransform);
+                        render.sortingOrder = Mathf.CeilToInt(-sort + _offset);
                     }
                 }
                 _worldPosition = pos;
@@ -61,7 +80,21 @@ namespace SFramework.Sprites
                 //     _worldPosition = pos;
                 // }
             }
+        }
+        
+        private float caculateSort(Transform camera)
+        {
+            float sort;
+            if (_sortAxis == SAxis.X)
+                sort = Mathf.Abs(_cameraTransform.position.x - transform.position.x) * 16;
+            else if (_sortAxis == SAxis.Y)
+                sort = Mathf.Abs(_cameraTransform.position.y - transform.position.y) * 16;
+            else
+                sort = Mathf.Abs(_cameraTransform.position.z - transform.position.z) * 16;
 
+            return sort;
         }
     }
+    
+
 }
