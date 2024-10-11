2de8d80ccd175c6bbc234c7f31fa83e482288e5a
bin.sun <pizzaman@126.com>
2024-10-11 14:54:50 +0800

    [框架] 增加摄像机抖动效果

---
+++ Assets/Plugins/SFramework/Script/Tools/CameraTools.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class CameraTools
+{
+    public async UniTask ShakeCameraAsync(float duration = 0.3f, float magnitude = 0.1f)
+    {
+        await ShakeCamera(duration, magnitude).ToUniTask();
+    }
+    
+    private IEnumerator ShakeCamera(float duration, float magnitude)
+    {
+        var mainCamera = Camera.main;
+        if (mainCamera != null)
+        {
+            Vector3 originalPos = mainCamera.transform.localPosition;
+
+            float elapsed = 0.0f;
+
+            while (elapsed < duration)
+            {
+                float x = UnityEngine.Random.Range(-1f, 1f) * magnitude + originalPos.x;
+                float y = UnityEngine.Random.Range(-1f, 1f) * magnitude + originalPos.y;
+            
+                mainCamera.transform.localPosition = new Vector3(x, y, originalPos.z);
+
+                elapsed += Time.deltaTime;
+
+                yield return null;
+            }
+
+            mainCamera.transform.localPosition = originalPos;
+        }
+    }
+}
